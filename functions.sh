251fe31b5ce6edcc962fb6fc99d8a0e6a4673d40
Yoseph Radding <raddingy@msu.edu>
2016-11-10 10:29:27 -0500

    Added update functionality to be able to update the program by running shuttl update

--- functions.sh
+++ functions.sh
@@ -6,6 +6,11 @@ shuttl() {
         sh $SHUTTL_DIR/run.sh 
         return
     fi
+    if test "$1" == "update"
+    then
+        sh $SHUTTL_DIR/update.sh 
+        return
+    fi
     source $SHUTTL_DIR/shuttlVenv/bin/activate
     python $SHUTTL_DIR/run.py $*
     deactivate
